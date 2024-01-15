63db910424de511b5ef6701240ea29fd5cf93c28
tyleradams2002 <tylerjadams2002@gmail.com>
2024-01-14 20:36:46 -0500

    Initalization of GitHub Repo from GitBash

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import ParticlesBg from 'particles-bg';
+import React from 'react';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
+import SignIn from './components/SignIn/SignIn.js';
+import Register from './components/Register/Register';
+
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin',
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: ''
+            }
+        }
+    }
+
+    loadUser = (data) => {
+        this.setState({user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined
+            }})
+    }
+
+    calculateFaceLocation = (data) => {
+        const boundingBox = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.querySelector("#inputImage");
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: boundingBox.left_col * width,
+            topRow: boundingBox.top_row * height,
+            rightCol: width - (boundingBox.right_col * width),
+            bottomRow: height - (boundingBox.bottom_row * height),
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box})
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        fetch('http://localhost:3000/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response) {
+                    fetch('http://localhost:3000/image', {
+                        method: 'put',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            id: this.state.user.id
+                        })
+                    })
+                        .then(response => response.json())
+                        .then(count => {
+                            this.setState(Object.assign(this.state.user, { entries: count}))
+                        })
+                        .catch(console.log)
+
+                }
+                this.displayFaceBox(this.calculateFaceLocation(response))
+            })
+            .catch(err => console.log(err));
+    }
+
+    onRouteChange = (route) => {
+        if (route === 'signout') {
+            this.setState(initialState)
+        } else if (route === 'home') {
+            this.setState({isSignedIn: true});
+        }
+        this.setState({route: route});
+    }
+
+    render() {
+        const { isSignedIn, imageUrl, route, box } = this.state;
+        return (
+            <div className="App">
+                <ParticlesBg type="cobweb" num={150} bg={true} />
+                <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+                { route === 'home'
+                    ? <div>
+                        <Logo/>
+                        <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+                        <ImageLinkForm onInputChange={this.onInputChange}
+                                       onButtonSubmit={this.onButtonSubmit}/>
+                        <FaceRecognition box={box} imageUrl={imageUrl}/>
+                    </div>
+                    : (
+                        route === 'signin'
+                            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+/*
+
+Clarifai Notes
+Create an App on Clarifai and get the API Key in App Settings and paste the key in "apiKey" object property
+IF we get a Model Does Not exist error, onButtonSubmit uses predict and has changed to use the new Model ID for the model we are wanting to use
+
+ */
